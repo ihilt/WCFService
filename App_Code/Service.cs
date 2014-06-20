2450051b7e98cef4b995e540c13e8ecfea9d88c8
Ian Hilt <ihilt@cbccts.org>
2014-06-19 20:07:12 -0400

    Project files
    Signed-off-by: Ian Hilt <ihilt@cbccts.org>

---
+++ App_Code/Service.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Web.Script.Serialization;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service" in code, svc and config file together.
+public class Service : IService
+{
+	public string GetData(int value)
+	{
+        var s = new JavaScriptSerializer();
+        return s.Serialize(new { value });
+	}
+
+	public CompositeType GetDataUsingDataContract(CompositeType composite)
+	{
+		if (composite == null)
+		{
+			throw new ArgumentNullException("composite");
+		}
+		if (composite.BoolValue)
+		{
+			composite.StringValue += "Suffix";
+		}
+		return composite;
+	}
+}
